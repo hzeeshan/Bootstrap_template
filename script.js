c9e1fb4bb7300b2951e09477ec44ca58f0db3880
Zeeshan <hafizzeeshan619@gmail.com>
2017-10-30 13:58:02 +0100

    Added Bootstrap Band Template

---
+++ script.js
@@ -0,0 +1,18 @@
+// getting the Geo location
+
+/*function myMap() {
+var myCenter = new google.maps.LatLng(45.070339, 7.686864);
+var mapProp = {center:myCenter, zoom:12, scrollwheel:false, draggable:false, mapTypeId:google.maps.MapTypeId.ROADMAP};
+var map = new google.maps.Map(document.getElementById("googleMap"),mapProp);
+var marker = new google.maps.Marker({position:myCenter});
+marker.setMap(map);
+}
+
+*/
+
+
+
+$(document).ready(function(){
+    // Initialize Tooltip
+    $('[data-toggle="tooltip"]').tooltip(); 
+})
